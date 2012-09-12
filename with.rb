b497aa904f51d5a2c9bea65b3eb8a358efc77968
Haydn Ewers <haydn@katalyst.com.au>
2012-09-12 20:56:48 +0930

    Update to "with" v0.1.0.

--- with.rb
+++ with.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class With < Formula
   homepage 'https://github.com/ketchup/with'
-  url 'https://github.com/ketchup/with/zipball/master'
+  url 'https://github.com/ketchup/with/zipball/v0.1.0'
   sha1 '97723cce3a85b9d0cb04e1c0b84e103170e545e2'
-  version '0.0.0'
 
   def install
     bin.install 'bin/with' => 'with'
