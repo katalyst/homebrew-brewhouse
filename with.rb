80e2574deea8ee88af2ea5065497d6d4dd4bb671
Haydn Ewers <haydn@katalyst.com.au>
2012-11-02 15:11:06 +1030

    Add test to with formula.

--- with.rb
+++ with.rb
@@ -5,6 +5,10 @@ class With < Formula
   url 'https://github.com/ketchup/with/zipball/v0.1.0'
   sha1 '97723cce3a85b9d0cb04e1c0b84e103170e545e2'
 
+  def test
+    system "with"
+  end
+
   def install
     bin.install 'bin/with' => 'with'
     prefix.install Dir["environment", "template"]
