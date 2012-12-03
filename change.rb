93ccda793b1b7a8ddd3f896a67ac9229a0501113
Haydn Ewers <haydn@katalyst.com.au>
2012-12-03 18:14:54 +1030

    Update change to v1.2.

--- change.rb
+++ change.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Change < Formula
 
   homepage 'https://github.com/ketchup/change'
-  url 'https://github.com/ketchup/change/zipball/v1.1.0'
-  sha1 '9cd85bf812c5c0b76067af0db70b53ac67a8d069'
+  url 'https://github.com/ketchup/change/zipball/v1.2.0'
+  sha1 'cebfa1dd285a67ad2326572f083326ec9c067402'
 
   def test
     system "change"
