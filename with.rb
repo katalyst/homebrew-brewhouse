d1d915a4a85c06f9dfbdb51496693250bd9f864c
Haydn Ewers <haydn@katalyst.com.au>
2013-06-07 15:00:37 +0930

    Fix the with SHA1.

--- with.rb
+++ with.rb
@@ -3,7 +3,7 @@ require 'formula'
 class With < Formula
   homepage 'https://github.com/ketchup/with'
   url 'https://github.com/ketchup/with/zipball/v0.1.0'
-  sha1 '97723cce3a85b9d0cb04e1c0b84e103170e545e2'
+  sha1 '38e1709dfd1e7e4469d7360923ca8e077c5393d5'
 
   def test
     system "with"
